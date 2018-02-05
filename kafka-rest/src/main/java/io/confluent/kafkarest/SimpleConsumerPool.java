5c53ca5e37c2e26603ee047c4620aa498427fe88
alinaabramova <aabramova@maprtech.com>
2018-02-05 12:09:53 +0200

    KAFKA-60 Fixed consumer pool for consuming by partitions

--- kafka-rest/src/main/java/io/confluent/kafkarest/SimpleConsumerPool.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/SimpleConsumerPool.java
@@ -13,25 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
 package io.confluent.kafkarest;
 
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
-
-import kafka.javaapi.consumer.SimpleConsumer;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * The SizeLimitedSimpleConsumerPool keeps a pool of SimpleConsumers
  * and can increase the pool within a specified limit
  */
 public class SimpleConsumerPool {
-
   private static final Logger log = LoggerFactory.getLogger(SimpleConsumerPool.class);
 
   // maxPoolSize = 0 means unlimited
@@ -41,69 +37,138 @@ public class SimpleConsumerPool {
   private final Time time;
 
   private final SimpleConsumerFactory simpleConsumerFactory;
-  private final Map<String, SimpleConsumer> simpleConsumers;
-  private final Queue<String> availableConsumers;
+  private final Map<String, TPConsumerState> simpleConsumers;
 
-  public SimpleConsumerPool(
-      int maxPoolSize,
-      int poolInstanceAvailabilityTimeoutMs,
-      Time time,
-      SimpleConsumerFactory simpleConsumerFactory
-  ) {
+  private final Queue<String> availableKafkaConsumers;
+  private final Queue<String> availableStreamsConsumers;
+  private final KafkaStreamsMetadataObserver metadataObserver;
+
+  private final Lock poolLock;
+
+  public SimpleConsumerPool(int maxPoolSize, int poolInstanceAvailabilityTimeoutMs,
+                            Time time, SimpleConsumerFactory simpleConsumerFactory, KafkaStreamsMetadataObserver metadataObserver) {
     this.maxPoolSize = maxPoolSize;
     this.poolInstanceAvailabilityTimeoutMs = poolInstanceAvailabilityTimeoutMs;
     this.time = time;
     this.simpleConsumerFactory = simpleConsumerFactory;
-
-    simpleConsumers = new HashMap<String, SimpleConsumer>();
-    availableConsumers = new LinkedList<String>();
+    this.metadataObserver = metadataObserver;
+    this.poolLock = new ReentrantLock(true);
+    simpleConsumers = new HashMap<String, TPConsumerState>();
+    availableKafkaConsumers = new LinkedList<String>();
+    availableStreamsConsumers = new LinkedList<String>();
   }
 
-  public synchronized SimpleFetcher get(final String host, final int port) {
+  /**
+   * @return assigned Consumer that is ready to be used for polling records
+   */
+  public synchronized TPConsumerState get(String topic, int partition) {
 
     final long expiration = time.milliseconds() + poolInstanceAvailabilityTimeoutMs;
 
     while (true) {
-      // If there is a SimpleConsumer available
-      if (availableConsumers.size() > 0) {
-        final String consumerId = availableConsumers.remove();
-        return new SimpleFetcher(simpleConsumers.get(consumerId), this);
+      // is the request for a given topic passed to Streams
+      boolean requestToStreams = metadataObserver.requestToStreams(topic);
+
+      String consumerId = null;
+      if (requestToStreams) {
+        // If there is a streams SimpleConsumer available
+        if (availableStreamsConsumers.size() > 0) {
+          consumerId = availableStreamsConsumers.remove();
+        }
+      } else {
+        // If there is a kafka SimpleConsumer available
+        if (availableKafkaConsumers.size() > 0) {
+          consumerId = availableKafkaConsumers.remove();
+        }
+      }
+
+      if (consumerId != null) {
+        TPConsumerState consumerState = simpleConsumers.get(consumerId);
+        consumerState.consumer().assign(Collections
+          .singletonList(new TopicPartition(topic, partition)));
+
+        return consumerState;
       }
 
       // If not consumer is available, but we can instantiate a new one
       if (simpleConsumers.size() < maxPoolSize || maxPoolSize == 0) {
-        final SimpleConsumer simpleConsumer = simpleConsumerFactory.createConsumer(host, port);
-        simpleConsumers.put(simpleConsumer.clientId(), simpleConsumer);
-        return new SimpleFetcher(simpleConsumer, this);
+        return createAndAssign(topic, partition, requestToStreams);
       }
 
       // If no consumer is available and we reached the limit
       try {
-        // The behavior of wait when poolInstanceAvailabilityTimeoutMs=0 is consistent as it
-        // won't timeout
+        // The behavior of wait when poolInstanceAvailabilityTimeoutMs=0 is consistent as it won't timeout
         wait(poolInstanceAvailabilityTimeoutMs);
       } catch (InterruptedException e) {
         log.warn("A thread requesting a SimpleConsumer has been interrupted while waiting", e);
       }
 
-      // In some cases ("spurious wakeup", see wait() doc), the thread will resume before the
-      // timeout
+      // In some cases ("spurious wakeup", see wait() doc), the thread will resume before the timeout
       // We have to guard against that and throw only if the timeout has expired for real
       if (time.milliseconds() > expiration && poolInstanceAvailabilityTimeoutMs != 0) {
-        throw Errors.simpleConsumerPoolTimeoutException();
+        boolean removed = false;
+        try {
+          // Since we cannot reuse a consumer for different backends once it was assigned
+          // we will remove available consumer from another backend (if such exists) and
+          // create new one for requested backend.
+          if (requestToStreams) {
+            if (availableKafkaConsumers.size() > 0) {
+              TPConsumerState removedConsumer = simpleConsumers.remove(availableKafkaConsumers.remove());
+              removedConsumer.consumer().close();
+              removed = true;
+            }
+          } else {
+            if (availableStreamsConsumers.size() > 0) {
+              TPConsumerState removedConsumer = simpleConsumers.remove(availableStreamsConsumers.remove());
+              removedConsumer.consumer().close();
+              removed = true;
+            }
+          }
+        } catch (Exception e) {
+          log.warn("Exception while closing consumer", e);
+        }
+
+        if (removed) {
+          return createAndAssign(topic, partition, requestToStreams);
+        } else {
+          throw Errors.simpleConsumerPoolTimeoutException();
+        }
       }
     }
   }
 
-  public synchronized void release(SimpleFetcher simpleFetcher) {
-    log.debug("Releasing into the pool SimpleConsumer with id " + simpleFetcher.clientId());
-    availableConsumers.add(simpleFetcher.clientId());
+  private synchronized TPConsumerState createAndAssign(String topic, int partition, boolean isStreamsConsumer) {
+    final SimpleConsumerFactory.ConsumerProvider simpleConsumer = simpleConsumerFactory.createConsumer();
+
+    // assign consumer to TopicPartition
+    // depending on topic string and default streams presence
+    // KafkaConsumer is marked as Streams or Kafka consumer.
+    // The variable isStreamsConsumer must be set to the correct
+    // value that corresponds to topic
+    simpleConsumer.consumer().assign(Collections
+      .singletonList(new TopicPartition(topic, partition)));
+
+    TPConsumerState consumerState =
+      new TPConsumerState(simpleConsumer.consumer(), isStreamsConsumer, this, simpleConsumer.clientId());
+    simpleConsumers.put(simpleConsumer.clientId(), consumerState);
+    return consumerState;
+  }
+
+
+  synchronized public void release(TPConsumerState tpConsumerState) {
+    log.debug("Releasing into the pool SimpleConsumer with id " + tpConsumerState.clientId());
+    if (tpConsumerState.isStreams()) {
+      availableStreamsConsumers.add(tpConsumerState.clientId());
+    } else {
+      availableKafkaConsumers.add(tpConsumerState.clientId());
+    }
     notify();
   }
 
   public void shutdown() {
-    for (SimpleConsumer simpleConsumer : simpleConsumers.values()) {
-      simpleConsumer.close();
+    for (TPConsumerState consumer : simpleConsumers.values()) {
+      consumer.consumer().wakeup();
+      consumer.consumer().close();
     }
   }
 
