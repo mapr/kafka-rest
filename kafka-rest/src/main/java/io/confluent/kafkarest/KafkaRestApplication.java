f21cf84a38724204fa79d2f32254b05fa5ee7431
alinaabramova <aabramova@maprtech.com>
2018-02-05 13:08:28 +0200

    KAFKA-61 Add API for topic list for v2

--- kafka-rest/src/main/java/io/confluent/kafkarest/KafkaRestApplication.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/KafkaRestApplication.java
@@ -127,6 +127,7 @@ public class KafkaRestApplication extends Application<KafkaRestConfig> {
     config.register(new PartitionsResource(context));
     config.register(new ConsumersResource(context));
     config.register(new StreamsResource(context));
+    config.register(new  io.confluent.kafkarest.resources.v2.StreamsResource((context)));
     config.register(new io.confluent.kafkarest.resources.v2.ConsumersResource(context));
     config.register(new io.confluent.kafkarest.resources.v2.PartitionsResource(context));
     config.register(KafkaRestCleanupFilter.class);
