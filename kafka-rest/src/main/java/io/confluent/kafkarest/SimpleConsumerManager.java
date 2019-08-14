98fd294b4574ca249755973b3223283babea91b9
Vyacheslav Rud <vrud@mapr.com>
2019-08-15 00:22:44 +0300

    KAFKA-444 Remove outdated caching mechanism

--- kafka-rest/src/main/java/io/confluent/kafkarest/SimpleConsumerManager.java
+++ kafka-rest/src/main/java/io/confluent/kafkarest/SimpleConsumerManager.java
@@ -21,10 +21,14 @@ import io.confluent.kafkarest.entities.BinaryConsumerRecord;
 import io.confluent.kafkarest.entities.ConsumerRecord;
 import io.confluent.kafkarest.entities.EmbeddedFormat;
 import io.confluent.kafkarest.entities.JsonConsumerRecord;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -45,6 +49,7 @@ public class SimpleConsumerManager {
   private static final Logger log = LoggerFactory.getLogger(SimpleConsumerManager.class);
 
   private final int maxPoolSize;
+  private final int maxPollTime;
   private final int poolInstanceAvailabilityTimeoutMs;
   private final Time time;
 
@@ -69,6 +74,7 @@ public class SimpleConsumerManager {
     this.simpleConsumerFactory = simpleConsumerFactory;
 
     maxPoolSize = config.getInt(KafkaRestConfig.SIMPLE_CONSUMER_MAX_POOL_SIZE_CONFIG);
+    maxPollTime = config.getInt(KafkaRestConfig.SIMPLE_CONSUMER_MAX_POLL_TIME_CONFIG);
     poolInstanceAvailabilityTimeoutMs =
             config.getInt(KafkaRestConfig.SIMPLE_CONSUMER_POOL_TIMEOUT_MS_CONFIG);
     time = config.getTime();
@@ -101,36 +107,50 @@ public class SimpleConsumerManager {
                       final EmbeddedFormat embeddedFormat,
                       final ConsumerManager.ReadCallback callback) {
 
-    List<ConsumerRecord> records = new ArrayList<>();
+    List<ConsumerRecord> result = new ArrayList<>();
     RestException exception = null;
 
     if (!mdObserver.topicExists(topicName)) {
-      exception = Errors.topicNotFoundException();
-    } else {
-      if (!mdObserver.partitionExists(topicName, partitionId)) {
-        exception = Errors.partitionNotFoundException();
-      } else {
-        try (TPConsumerState consumer = simpleConsumersPool.get(topicName, partitionId)) {
-          List<org.apache.kafka.clients.consumer.ConsumerRecord<byte[], byte[]>> fetched =
-              cache.pollRecords(consumer.consumer(), topicName, partitionId, offset, count);
-
-          for (org.apache.kafka.clients.consumer.ConsumerRecord record: fetched) {
-            records.add(createConsumerRecord(record, record.topic(),
-                    record.partition(), embeddedFormat));
+      callback.onCompletion(null, Errors.topicNotFoundException());
+      return;
+    }
+    if (!mdObserver.partitionExists(topicName, partitionId)) {
+      callback.onCompletion(null, Errors.partitionNotFoundException());
+      return;
+    }
+    try (TPConsumerState consumer = simpleConsumersPool.get(topicName, partitionId)) {
+      Consumer<byte[], byte[]> assignedConsumer = consumer.consumer();
+      long pollTimeout = maxPollTime;
+      assignedConsumer.seek(new TopicPartition(topicName, partitionId), offset);
+      long endTime = time.milliseconds() + pollTimeout;
+      Iterator<org.apache.kafka.clients.consumer.ConsumerRecord<byte[], byte[]>> it = null;
+      boolean enough = false;
+      while (!enough && (pollTimeout = endTime - time.milliseconds()) > 0) {
+        if (it == null || !it.hasNext()) {
+          ConsumerRecords<byte[], byte[]> records = assignedConsumer.poll(Math.max(0, pollTimeout));
+          if (records.isEmpty()) {
+            continue;
           }
-
-        } catch (Throwable e) {
-          if (e instanceof RestException) {
-            exception = (RestException) e;
-          } else {
-            exception = Errors.kafkaErrorException(e);
-            log.warn("Internal error", e);
+          it = records.iterator();
+        }
+        while (it.hasNext()) {
+          org.apache.kafka.clients.consumer.ConsumerRecord<byte[], byte[]> record = it.next();
+          result.add(createConsumerRecord(record, topicName, partitionId, embeddedFormat));
+          if (result.size() == count) {
+            enough = true;
+            break;
           }
         }
       }
+    } catch (Throwable e) {
+      if (e instanceof RestException) {
+        exception = (RestException) e;
+      } else {
+        exception = Errors.kafkaErrorException(e);
+        log.warn("Internal error", e);
+      }
     }
-
-    callback.onCompletion(records, exception);
+    callback.onCompletion(result, exception);
   }
 
   private BinaryConsumerRecord createBinaryConsumerRecord(
